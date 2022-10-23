90398f6a54b346077ede6d700eb27d37713db65a
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-10-23 18:30:11 +0530

    update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import telebot
 
 TOKEN = '2109918986:AAHSLdqv_4CwQc8GFSSYTQShjSSHK8tutXs' 
 bot = telebot.TeleBot(TOKEN) 
-server = Flask(__name__) 
+app = Flask(__name__) 
 #app = Flask(__name__)
 
 @bot.message_handler(commands=['start']) 
@@ -16,7 +16,7 @@ def echo_message(message):
   bot.reply_to(message, message.text) 
   
   
-@server.route('/' + TOKEN, methods=['POST']) 
+@app.route('/' + TOKEN, methods=['POST']) 
 def getMessage(): 
   json_string = request.get_data().decode('utf-8') 
   update = telebot.types.Update.de_json(json_string) 
@@ -24,17 +24,17 @@ def getMessage():
   return "!", 200 
   
   
-#@server.route("/") 
+#@app.route("/") 
 #def webhook(): 
   #bot.remove_webhook() 
   #bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN) 
   #return "!", 200
 
 
-@server.route("/home")
+@app.route("/home")
 def hello_world():
   return "kahsj djdk"
   #return render_template("index.html")
 
 if __name__ == "__main__":
-  server.run(host="0.0.0.0",port=int(os.environ.get('PORT',5000))) 
+  app.run(host="0.0.0.0",port=int(os.environ.get('PORT',5000))) 
