01d1a18e982cd23c5889d9afe150c0a5daa830ef
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2022-10-31 14:07:15 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask
 from flask import render_template
-import telebot 
+#import telebot 
 
 TOKEN = '2109918986:AAGoz-c4sw5zjtDeq9C2875GiV_4S4rQya4' 
-bot = telebot.TeleBot(TOKEN) 
+#bot = telebot.TeleBot(TOKEN) 
 app = Flask(__name__) 
 #app = Flask(__name__)
 
-@bot.message_handler(commands=['start']) 
-def start(message):
-  bot.reply_to(message, 'Hello, ' + message.from_user.first_name) 
+#@bot.message_handler(commands=['start']) 
+#def start(message):
+  #bot.reply_to(message, 'Hello, ' + message.from_user.first_name) 
  
-@bot.message_handler(func=lambda message: True, content_types=['text']) 
-def echo_message(message):
-  bot.reply_to(message, message.text) 
+#@bot.message_handler(func=lambda message: True, content_types=['text']) 
+#def echo_message(message):
+  #bot.reply_to(message, message.text) 
   
   
 #@app.route('/' + TOKEN, methods=['POST']) 
@@ -36,11 +36,11 @@ def hello_world():
   #return "kahsj djdk"
   return render_template("index.html")
 
-bot.polling()
+#bot.polling()
 
-if __name__ == "__main__":
-  print("okay")
-  app.run()
-  print("okay1")
+#if __name__ == "__main__":
+  #print("okay")
+  #app.run()
+  #print("okay1")
   #bot.polling()
   #app.run(host="0.0.0.0",port=int(os.environ.get('PORT',5000))) 
